490a8236f3dd15e2f143f6e135fb57491fbc2b91
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-19 03:14:55 +0530

    chore: Add analytics component to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
+
 const rootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       
       {children}
-      
+      <Analytics />
     </main>
   );
 };
