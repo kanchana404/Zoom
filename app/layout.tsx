10aa08583e3c80035d99cc85dfec3f0d80ccb917
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-20 03:00:14 +0530

    chore: Add @radix-ui/react-dropdown-menu dependency to package.json and update RootLayout with new styles and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,46 @@
+import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/react";
-import { Toaster } from "@/components/ui/toaster"
+import { Inter } from "next/font/google";
 
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yoom",
-  description: "Use this webapp instead Zoom",
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
       <ClerkProvider
         appearance={{
           layout: {
-            logoImageUrl: "/icons/yoom-logo.svg",
             socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
           },
-
           variables: {
             colorText: "#fff",
             colorPrimary: "#0E78F9",
-            colorBackground: "#1c1f2c",
-            colorInputBackground: "#252a41",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
             colorInputText: "#fff",
           },
         }}
       >
         <body className={`${inter.className} bg-dark-2`}>
-          {children}
           <Toaster />
-          <Analytics />
+          {children}
         </body>
       </ClerkProvider>
     </html>
