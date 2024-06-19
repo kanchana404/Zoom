d052d2e6fd0a9e1b6e1253027628ed0f38cbdaa7
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-19 14:04:18 +0530

    chore: Update homepage with upcoming meeting time and display time in Sri Lanka timezone

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,15 +3,16 @@ import React from 'react'
 const Home = () => {
 
   const now = new Date();
+const timeInSriLanka = now.toLocaleTimeString('en-US', { timeZone: 'Asia/Colombo' });
+console.log(timeInSriLanka);
 
-  const time = now.toLocaleTimeString();
   const date = '';
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
   <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
    <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-    <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meating at:{time}</h2>
+    <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meating at:{timeInSriLanka}</h2>
     <div className='flex flex-col gap-2'>
       <h1 className='text-4xl font-extralight lg:text-7xl'>11.30AM</h1>
       <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
